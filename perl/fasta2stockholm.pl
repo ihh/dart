4b19caba71f266011167a816edd06b2b60ff81b4
Ian Holmes <yam@rosebud.local>
2009-03-31 17:04:46 -0700

    fasta2stockholm.pl

--- perl/fasta2stockholm.pl
+++ perl/fasta2stockholm.pl
@@ -16,44 +16,47 @@ push @argv, "-" unless @argv;
 
 # loop through FASTA files
 foreach my $fasta (@argv) {
+# print Stockholm output
+    print "# STOCKHOLM 1.0\n";
+
 # read FASTA file
-    my %seq;
+    my %len;
     my @name;
     my $name;
     open FASTA, "<$fasta" or die "Couldn't open '$fasta': $!";
     while (<FASTA>) {
 	if (/^\s*>\s*(\S+)/) {
+	    print "\n" if defined $name;
 	    $name = $1;
+	    die "Duplicate sequence name: $name\n" if exists $len{$name};
+	    print "$name ";
 	    push @name, $name;
 	} else {
 	    if (/\S/ && !defined $name) {
 		warn "Ignoring: $_";
 	    } else {
 		s/\s//g;
-		$seq{$name} .= $_;
+		print;
+		$len{$name} += length;
 	    }
 	}
     }
     close FASTA;
+    print "\n" if defined $name;
+    print "//\n";
+
+    warn map (" $_ : $len{$_}\n", @name);
 
 # check all seqs are same length
     my $length;
-    my $lname;
     foreach my $name (@name) {
-	my $l = length $seq{$name};
+	my $l = $len{$name};
 	if (defined $length) {
-	    die "Sequences not all same length ($lname is $length, $name is $l)" unless $length == $l;
+	    if ($length != $l) {
+		die "Sequences not all same length:\n", map (" $_ : $len{$_}\n", @name);
+	    }
 	} else {
-	    $length = length $seq{$name};
-	    $lname = $name;
+	    $length = $l;
 	}
     }
-
-# print Stockholm output
-    print "# STOCKHOLM 1.0\n";
-    foreach my $name (@name) {
-	print $name, " ", $seq{$name}, "\n";
-    }
-    print "//\n";
 }
-
