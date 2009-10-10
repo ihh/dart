b4198637cc6344680bd11b0c4a63dff4475f09d9
Ian Holmes <yam@emin.local>
2009-10-10 15:10:25 -0700

    Removed more dead code

--- telegraph/list_assignment.h
+++
@@ -1,8 +0,0 @@
-/* Fn for list assignment, a modification to the grammar */
-
-
-typedef tg_vector(double*) tg_assign_vec;
-typedef tg_vector(int) tg_int_vector;
-
-tg_assign_vec tg_return_assign_vec(double prob_const);
-void tg_list_assign_prob(tg_grammar* tg, tg_prob* prob, tg_assign_vec prob_vec);
