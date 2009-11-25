822dd0eb7992af9a9327bf65e9990fe2f498b31f
Ian Holmes <yam@rosebud.local>
2009-11-25 14:50:02 -0800

    comments re Scheme garbage collection

--- src/guile/dpm-type.h
+++ src/guile/dpm-type.h
@@ -11,10 +11,11 @@ SCM make_xrate_dpm_smob (const ECFG_scores& ecfg, const Stockholm& stock);
 
 // guile smobs
 struct XRATE_DPM_smob {
-  // Scheme objects
+  // Scheme objects; we need to mark these when we are marked
   SCM stock_smob_scm;
   // internal data
   // these should be pointers to avoid potential deep-copy bugs (although there shouldn't be any deep links here anyway)
+  Stockholm_smob* stock;  // this is not a deep-copy, it is the marked stock_smob_scm
   ECFG_scores* ecfg;
   ECFG_envelope* env;
   PCounts* pcounts;
