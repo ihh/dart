2fd3b3a96baaf64f8589dbc58bcea72e8c644990
Ian Holmes <yam@rosebud.local>
2009-12-03 15:36:40 -0800

    Added stub main file for beagle version of xrate

---
+++ src/beagle/xrate-beagle.cc
@@ -0,0 +1,49 @@
+#include "ecfg/ecfgmain.h"
+#include "ecfg/pfold.h"
+#include "ecfg/protgrammar.h"
+
+// TODO: uncomment the following line, and #define BEAGLE_INCLUDE_PATH somewhere
+// #include BEAGLE_INCLUDE_PATH "/beagle.h"
+
+typedef ECFG_main::ECFG_map ECFG_map;
+
+// main program
+int main (int argc, char* argv[])
+{
+  // create main xrate program object
+  // TODO: define ECFG_main-derived class that uses Beagle, and use that class here instead of ECFG_main
+  ECFG_main ecfg_main (argc, argv);
+
+  // initialise grammars
+  ecfg_main.add_grammar ("rev", new Null_ECFG (DNA_alphabet, true));
+  ecfg_main.add_grammar ("irrev", new Null_ECFG (DNA_alphabet, false));
+  ecfg_main.add_grammar ("dinuc", new Nearest_neighbor_ECFG());
+  ecfg_main.add_grammar ("codon", new Codon_ECFG (true));
+  ecfg_main.add_grammar ("aa", new Protein_grammar(1,1));
+  ecfg_main.add_grammar ("aa2", new Protein_grammar(1,2));
+  ecfg_main.add_grammar ("aa3", new Protein_grammar(1,3));
+  ecfg_main.add_grammar ("aa4", new Protein_grammar(1,4));
+  ecfg_main.add_grammar ("pfold", new PFOLD_ECFG());
+
+  // set default grammars
+  ecfg_main.default_grammars = "rev";
+
+  // initialise the options parser
+  ecfg_main.init_opts ("annotate alignments and estimate evolutionary rates\n");
+
+  // run
+  try {
+    ecfg_main.run_xrate (cout);
+  } catch (const Dart_exception& e)
+    {
+      CLOGERR << "ERROR: " << e.what();
+      exit(1);
+    }
+
+  // delete grammars
+  for_contents (ECFG_map, ecfg_main.ecfg_map, sg)
+    delete (*sg).second;
+
+  // return
+  return 0;
+}
