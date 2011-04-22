1d1c8b27c8361a5a59d2c8f586057e6816539fbe
Oscar Westesson <oscar@rigel.biowiki.org>
2011-04-22 15:52:53 -0700

    Import/export module added

---
+++ src/protpal/ImportExport.h
@@ -0,0 +1,11 @@
+#ifndef IMPORTEXPORT_H
+#define IMPORTEXPORT_H
+
+#include<iostream>
+#include<map>
+#include<vector>
+
+#include "protpal/profile.h" 
+// soon include DART libs like sexpr, etc... */
+using namespace std;
+#endif
