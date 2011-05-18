9e2fd9d7ca3fa2efb4709f5e3007bb2fa4ed9741
Ian Holmes <yam@static-169-229-195-136.qb3.berkeley.edu>
2011-05-18 13:05:56 -0700

    guile/guile-*-keywords.h

--- src/guile/guile-keywords.h
+++ src/guile/guile-keywords.h
@@ -1,8 +1,8 @@
 #ifndef GUILE_KEYWORDS_INCLUDED
 #define GUILE_KEYWORDS_INCLUDED
 
-#include "guile-newick-keywords.h"
-#include "guile-stockholm-keywords.h"
-#include "guile-xrate-keywords.h"
+#include "guile/guile-newick-keywords.h"
+#include "guile/guile-stockholm-keywords.h"
+#include "guile/guile-xrate-keywords.h"
 
 #endif /* GUILE_KEYWORDS_INCLUDED */
