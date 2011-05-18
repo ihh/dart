0471c611648d5b8cae9e2888c4bedaa6a42e0dcf
Ian Holmes <yam@static-169-229-195-136.qb3.berkeley.edu>
2011-05-18 13:03:45 -0700

    guile/guile-*-keywords.h

---
+++ src/guile/guile-newick-keyworks.h
@@ -0,0 +1,14 @@
+#ifndef GUILE_NEWICK_KEYWORDS_INCLUDED
+#define GUILE_NEWICK_KEYWORDS_INCLUDED
+
+#define GUILE_NEWICK_FROM_STRING                      "newick-from-string"
+#define GUILE_NEWICK_FROM_FILE                        "newick-from-file"
+#define GUILE_NEWICK_FROM_STOCKHOLM                   "newick-from-stockholm"
+#define GUILE_NEWICK_TO_FILE                          "newick-to-file"
+#define GUILE_NEWICK_ANCESTOR_LIST                    "newick-ancestor-list"
+#define GUILE_NEWICK_LEAF_LIST                        "newick-leaf-list"
+#define GUILE_NEWICK_BRANCH_LIST                      "newick-branch-list"
+#define GUILE_NEWICK_UNPACK                           "newick-unpack"
+
+#endif /* GUILE_NEWICK_KEYWORDS_INCLUDED */
+
