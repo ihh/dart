a10443a20a922fe9407a45f7fc458f6575e0d650
Ian Holmes <yam@static-169-229-195-136.qb3.berkeley.edu>
2011-05-19 12:34:04 -0700

    force update-policy parametric on model-scheme models

--- src/ontology/onto_keywords.h
+++ src/ontology/onto_keywords.h
@@ -2,6 +2,7 @@
 #define ONTO_KEYWORDS_INCLUDED
 
 #define TERMINATRIX               "terminatrix"
+#define TERMINATRIX_PARAMS        "params"
 #define TERMINATRIX_MODEL         "model"
 #define TERMINATRIX_MODEL_SCM     "model-scheme"
 #define TERMINATRIX_INIT_SCM      "init-scheme"
--- src/ontology/onto_sexpr.h
+++ src/ontology/onto_sexpr.h
@@ -8,8 +8,11 @@
 #endif
 
 #include "ecfg/ecfgsexpr.h"
+#include "seq/psexpr.h"
 #include "util/svisitor.h"
 
+
+// Terminatrix class
 struct Terminatrix
 {
   // primary object interface to Scheme
@@ -18,13 +21,22 @@ struct Terminatrix
   // SExpr's
   SExpr init_sexpr, model_sexpr, tree_db_sexpr, knowledge_sexpr;
 
-  // model
-  const Alphabet dummy_alph;
-  list<Alphabet> alph_list;
-  Alphabet_dictionary alph_dict;
+  // symbol tables
+  PFunc_builder::SymPVar sym2pvar;
+  PFunc_builder::SymIndex term2chain;
+
+  // params
   PScores pscores;
   PCounts pcounts, var_counts;
   set<int> mutable_pgroups;
+
+  // alphabet namespace
+  // (alph_list is a bit wasteful; we already store every Alphabet in alph_dict. Could just store a list of Alphabet names instead...)
+  const Alphabet dummy_alph;  // the ECFG_matrix_set needs a default Alphabet. Keep this around to pacify it
+  list<Alphabet> alph_list;
+  Alphabet_dictionary alph_dict;
+
+  // continuous-time Markov chain
   ECFG_matrix_set matrix_set;
 
   // results
@@ -37,13 +49,14 @@ struct Terminatrix
   Terminatrix (SExpr_Scheme_evaluator& scheme);  // the SExpr_Scheme_evaluator is assumed to have been initialized
 
   // helpers
-  void eval_funcs();
+  void eval_funcs();  // populates the ECFG_chain (owned by the ECFG_matrix_set) with numeric values, by evaluating functions
 
   // accessors
   ECFG_chain& chain();
   EM_matrix_base& rate_matrix();
 };
 
+// Terminatrix I/O adapter
 struct Terminatrix_builder : ECFG_builder
 {
   // load
@@ -51,13 +64,15 @@ struct Terminatrix_builder : ECFG_builder
   // save
   static void terminatrix2stream (ostream& out, Terminatrix& terminatrix);
 
-  // helpers
+  // input helpers
+  static void init_terminatrix_params (Terminatrix& terminatrix, SExpr& terminatrix_params_sexpr);
   static void init_terminatrix_model (Terminatrix& terminatrix, SExpr& terminatrix_model_sexpr);
   static void init_terminatrix_member_sexpr (SExpr& member_sexpr, SExpr& parent_sexpr, const char* tag);
 
+  // output helpers
+  static void terminatrix_params2stream (ostream& out, Terminatrix& terminatrix);
   static void terminatrix_model2stream (ostream& out, Terminatrix& terminatrix);
   static void terminatrix_member_sexpr2stream (ostream& out, SExpr& member_sexpr);
 };
-
 
 #endif /* ONTO_SEXPR_INCLUDED */
