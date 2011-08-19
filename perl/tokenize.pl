ec322148c579705003ee027a13240397428a994a
Ian Holmes <yam@minter-3.local>
2011-08-19 14:50:41 -0700

    tokenize

--- perl/tokenize.pl
+++ perl/tokenize.pl
@@ -34,25 +34,30 @@ my (%aa, %tok);
         'gta'=>'V',  'gca'=>'A',  'gaa'=>'E',  'gga'=>'G',
         'gtg'=>'V',  'gcg'=>'A',  'gag'=>'E',  'ggg'=>'G' );
 
+# !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+# Characters avoided in token set:
+# ( ) ; " ' (used by S-expression format)
+# - * ? . (used for alignment/reconstruction/ambiguity)
+# > (used by FASTA format)
 %tok = ( 'ttt'=>'F',  'tct'=>'S',  'tat'=>'Y',  'tgt'=>'C',
 	 'ttc'=>'f',  'tcc'=>'s',  'tac'=>'y',  'tgc'=>'c',
-	 'tta'=>'L',  'tca'=>'5',  'taa'=>':',  'tga'=>'!',
-	 'ttg'=>'l',  'tcg'=>'$',  'tag'=>';',  'tgg'=>'W',
+	 'tta'=>'L',  'tca'=>'5',  'taa'=>'=',  'tga'=>'!',
+	 'ttg'=>'l',  'tcg'=>'$',  'tag'=>'_',  'tgg'=>'W',
        
 	 'ctt'=>'<',  'cct'=>'P',  'cat'=>'H',  'cgt'=>'R',
 	 'ctc'=>'[',  'ccc'=>'p',  'cac'=>'h',  'cgc'=>'r',
-	 'cta'=>'(',  'cca'=>',',  'caa'=>'Q',  'cga'=>')',
-	 'ctg'=>'{',  'ccg'=>"'",  'cag'=>'q',  'cgg'=>'}',
+	 'cta'=>'{',  'cca'=>',',  'caa'=>'Q',  'cga'=>'+',
+	 'ctg'=>'/',  'ccg'=>'8',  'cag'=>'q',  'cgg'=>'}',
        
 	 'att'=>'I',  'act'=>'T',  'aat'=>'N',  'agt'=>'%',
 	 'atc'=>'i',  'acc'=>'t',  'aac'=>'n',  'agc'=>'#',
-	 'ata'=>'|',  'aca'=>'~',  'aaa'=>'K',  'aga'=>'/',
-	 'atg'=>'M',  'acg'=>'`',  'aag'=>'k',  'agg'=>'\\',
+	 'ata'=>'|',  'aca'=>'~',  'aaa'=>'K',  'aga'=>'@',
+	 'atg'=>'M',  'acg'=>'`',  'aag'=>'k',  'agg'=>']',
        
 	 'gtt'=>'V',  'gct'=>'A',  'gat'=>'D',  'ggt'=>'G',
 	 'gtc'=>'v',  'gcc'=>'a',  'gac'=>'d',  'ggc'=>'g',
-	 'gta'=>'^',  'gca'=>'@',  'gaa'=>'E',  'gga'=>'_',
-	 'gtg'=>'>',  'gcg'=>'&',  'gag'=>'e',  'ggg'=>'=' );
+	 'gta'=>'^',  'gca'=>'4',  'gaa'=>'E',  'gga'=>'9',
+	 'gtg'=>'7',  'gcg'=>'&',  'gag'=>'e',  'ggg'=>'6' );
 
 my $frame = 0;
 my $revcomp = 0;
@@ -67,13 +72,20 @@ GetOptions ("frame=i" => \$frame,
 my $trans_ref = $use_aa ? \%aa : \%tok;
 my %untok = map (($$trans_ref{$_} => $_), keys %$trans_ref);
 
+# Uncomment to check for duplicate tokens
+for my $c (keys %tok) { die $c unless $untok{$tok{$c}} eq $c }
+
 @ARGV = ("-") unless @ARGV;
 for my $filename (@ARGV) {
-    iterseq ($filename, $untokenize ? \&untokenize : \&tokenize);
+    iterseq ($filename,
+	     sub {
+		 my ($name, $seq) = @_;
+		 printseq ($name, $untokenize ? untokenize($seq) : tokenize($seq));
+	     });
 }
 
 sub tokenize {
-    my ($name, $seq) = @_;
+    my ($seq) = @_;
     $seq = lc $seq;
     if ($revcomp) { $seq = revcomp ($seq) }
     my $trans = "";
@@ -81,15 +93,15 @@ sub tokenize {
 	$codon = substr ($seq, $pos, 3);
 	if (exists $$trans_ref{$codon}) { $trans .= $$trans_ref{$codon} }
     }
-    printseq ($name, $trans);
+    return $trans;
 }
 
 sub untokenize {
-    my ($name, $seq) = @_;
+    my ($seq) = @_;
     my $untrans = "n" x $frame;
     for (my $pos = 0; $pos < length($seq); ++$pos) {
 	$token = substr ($seq, $pos, 1);
 	if (exists $untok{$token}) { $untrans .= $untok{$token} }
     }
-    printseq ($name, $untrans);
+    return $untrans;
 }
